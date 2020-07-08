6f15f29c83126a9c9039d61c8f0f12bf12abb879
CCBaxter84 <troyceclark@gmail.com>
2020-07-08 13:00:07 -0600

    Added comments

--- models/item.js
+++ models/item.js
@@ -1,6 +1,8 @@
+//import mongoose and pull Schema class from it
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Instantiate an Item Schema from Schema class
 const ItemSchema = new Schema({
     name: {
         type: String,
