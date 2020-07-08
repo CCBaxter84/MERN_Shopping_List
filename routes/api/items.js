7e6ef04cd336f6a2ef32049aba2ab632ff588dcf
CCBaxter84 <troyceclark@gmail.com>
2020-07-08 16:03:56 -0600

    added and tested ItemsRouter delete request

--- routes/api/items.js
+++ routes/api/items.js
@@ -12,6 +12,7 @@ router.get("/", async (req, res) => {
         const items = await Item.find().sort({ date: -1 });
         res.json(items);
     } catch(err) {
+        res.status(500);
         console.log(err);
     }
 });
@@ -21,9 +22,22 @@ router.post("/", async (req, res) => {
     const newItem = new Item({ name: req.body.name });
     try {
         const item = await newItem.save();
-        res.json(item);
+        res.status(201).send(`${item.name} added to database`);
     } catch(err) {
+        res.status(500);
         console.log(err);
+    }
+});
+
+//delete an item from the database
+router.delete("/:id", async (req, res) => {
+    try {
+        const itemToDelete = await Item.findById(req.params.id);
+        const item = await itemToDelete.delete();
+        res.status(200).send(`${item.name} deleted from database`)
+
+    } catch(err) {
+        res.status(500).send("Error deleting requested item");
     }
 });
 
