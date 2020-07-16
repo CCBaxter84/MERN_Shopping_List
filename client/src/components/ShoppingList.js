39f53bb62d2d52c64292104be3dfee1c08df8cab
CCBaxter84 <troyceclark@gmail.com>
2020-07-16 15:44:46 -0600

    Finished adding and deleting new item use cases

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -4,9 +4,10 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { ItemsContext } from "./ItemsContext";
 
 function ShoppingList() {
-    //import global state using context
+    //Import global state using context
     const { items, setItems, setIdToDelete } = useContext(ItemsContext);
 
+    //This function is for deleting a specific item from "items" in global state
     function deleteItem(id) {
         setIdToDelete(id);
         setItems(prev => prev.filter(item => item.id !== id))
